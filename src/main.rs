5fe74c444c94251fecac47b65b669f5ddbf414d9
Peter Delevoryas <peter@pjd.dev>
2022-10-23 21:15:20 -0700

    main: Add Uart trait

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,34 @@ use core::arch::global_asm;
 use core::ptr;
 use core::panic::PanicInfo;
 
-mod ast2600 {
-    pub const UART5: u32 = 0x1E78_4000;
+trait Uart {
+    const BASE_ADDR: u32;
+
+    fn tx(&mut self, b: u8) {
+        unsafe {
+            ptr::write_volatile(Self::BASE_ADDR as *mut u32, b as u32);
+        }
+    }
+
+    fn rx(&mut self) -> u8 {
+        let r = unsafe {
+            ptr::read_volatile(Self::BASE_ADDR as *const u32)
+        };
+        r as u8
+    }
+}
+
+pub struct Uart5;
+
+impl Uart for Uart5 {
+    const BASE_ADDR: u32 = 0x1e78_4000;
 }
 
 global_asm!(include_str!("start.S"));
 
 fn print(msg: &str) {
     for &b in msg.as_bytes() {
-        unsafe {
-            ptr::write(ast2600::UART5 as *mut u32, b as u32);
-        }
+        Uart5.tx(b);
     }
 }
 
