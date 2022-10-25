1d40ddff9d976fe80a9457df9aa4179b4350cc95
Troy Lee <troy_lee@aspeedtech.com>
2022-10-25 03:25:50 +0000

    Add tx check
    Signed-off-by: Troy Lee <troy_lee@aspeedtech.com>

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,22 @@ use core::panic::PanicInfo;
 
 trait Uart {
     const BASE_ADDR: u32;
+    const UART_FCR: u32;
+    const UART_LSR: u32;
+
+    fn init(&mut self) {
+        unsafe {
+            // Enable UART FIFO
+            ptr::write_volatile(Self::UART_FCR as *mut u32, 1 as u32);
+        }
+    }
+
+    fn tx_ready(&mut self) -> u8 {
+        let r = unsafe {
+            ptr::read_volatile(Self::UART_LSR as *const u32) & 0x40
+        };
+        r as u8
+    }
 
     fn tx(&mut self, b: u8) {
         unsafe {
@@ -26,20 +42,25 @@ pub struct Uart5;
 
 impl Uart for Uart5 {
     const BASE_ADDR: u32 = 0x1e78_4000;
+    const UART_FCR: u32 = 0x1e78_4008;
+    const UART_LSR: u32 = 0x1e78_4014;
 }
 
 global_asm!(include_str!("start.S"));
 
 fn print(msg: &str) {
     for &b in msg.as_bytes() {
+        while Uart5.tx_ready() == 0 {
+        }
         Uart5.tx(b);
     }
 }
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
+    Uart5.init();
     loop {
-        print("hello world\n");
+        print("hello world\r\n");
     }
 }
 
